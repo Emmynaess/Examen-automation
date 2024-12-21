ad768cadb6d3ef50a211f92fb45aa4f51aef8771
Emmy Naess <actions@github.com>
2024-12-21 13:55:52 +0100

    upload changed scripts

--- pipeline_scripts/split_data.py
+++ pipeline_scripts/split_data.py
@@ -1,31 +1,39 @@
+import os
 import pandas as pd
 
 def main():
     df = pd.read_excel("customer_data.xlsx")
 
     invalid_conditions = (
-        df["First Name"].str.contains(r'\d', na=False) |
-        ~df["Email"].str.contains("@", na=False) |
-        ~df["Phone"].str.startswith("+46", na=False) |
-        df["Streetname"].isin(["No Street"]) |
-        df["Postcode"].isin(["No Postcode"]) |
-        df["City"].isin(["No City"]) |
+        df["First Name"].str.contains(r'\d', na=False)
+        |
+        ~df["Email"].str.contains("@", na=False)
+        |
+        ~df["Phone"].str.startswith("+46", na=False)
+        |
+        df["Streetname"].isin(["No Street"])
+        |
+        df["Postcode"].isin(["No Postcode"])
+        |
+        df["City"].isin(["No City"])
+        |
         df["Municipality"].isin(["No Municipality"])
     )
 
     df_invalid = df[invalid_conditions]
     df_valid = df[~invalid_conditions]
 
-    df_valid.to_excel("customer_data_valid.xlsx", index=False)
-    df_invalid.to_excel("customer_data_invalid.xlsx", index=False)
+    os.makedirs("customer_data_valid", exist_ok=True)
+    os.makedirs("customer_data_invalid", exist_ok=True)
 
-    print("=================================")
-    print(f"Antal rader valid: {len(df_valid)}")
-    print(f"Antal rader invalid: {len(df_invalid)}")
+    df_valid.to_excel("customer_data_valid/customer_data_valid.xlsx", index=False)
+    df_invalid.to_excel("customer_data_invalid/customer_data_invalid.xlsx", index=False)
+
+    print(f"Totalt antal rader: {len(df)}")
+    print(f"Valid: {len(df_valid)} | Invalid: {len(df_invalid)}")
     print("Filer skapade:")
-    print(" - customer_data_valid.xlsx")
-    print(" - customer_data_invalid.xlsx")
-    print("=================================")
+    print("  - customer_data_valid/customer_data_valid.xlsx")
+    print("  - customer_data_invalid/customer_data_invalid.xlsx")
 
 if __name__ == "__main__":
     main()

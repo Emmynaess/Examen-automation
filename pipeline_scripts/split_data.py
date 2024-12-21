b52f4268fc97774527a9a93c51be265780331aaa
Emmy Naess <actions@github.com>
2024-12-21 16:37:42 +0100

    jkfd

--- pipeline_scripts/split_data.py
+++ pipeline_scripts/split_data.py
@@ -1,6 +1,65 @@
 import os
 import pandas as pd
 
+def format_phone_number(phone: str) -> str:
+    formatted_phone = phone.replace(" ", "")
+    return formatted_phone
+
+def main():
+    df = pd.read_excel("customer_data.xlsx")
+
+    df["Phone"] = df["Phone"].apply(format_phone_number)
+
+    invalid_conditions = (
+        df["First Name"].str.contains(r'\d', na=False)
+        |
+        ~df["Email"].str.contains("@", na=False)
+        |
+        ~df["Phone"].str.startswith("+46", na=False)
+        |
+        df["Streetname"].isin(["No Street"])
+        |
+        df["Postcode"].isin(["No Postcode"])
+        |
+        df["City"].isin(["No City"])
+        |
+        df["Municipality"].isin(["No Municipality"])
+    )
+
+    df_invalid = df[invalid_conditions]
+    df_valid = df[~invalid_conditions]
+
+    os.makedirs("customer_data_valid", exist_ok=True)
+    os.makedirs("customer_data_invalid", exist_ok=True)
+
+    df_valid.to_excel("customer_data_valid/customer_data_valid.xlsx", index=False)
+    df_invalid.to_excel("customer_data_invalid/customer_data_invalid.xlsx", index=False)
+
+    print(f"Totalt antal rader: {len(df)}")
+    print(f"Valid: {len(df_valid)} | Invalid: {len(df_invalid)}")
+    print("Filer skapade:")
+    print("  - customer_data_valid/customer_data_valid.xlsx")
+    print("  - customer_data_invalid/customer_data_invalid.xlsx")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------
+
+"""import os
+import pandas as pd
+
 def main():
     df = pd.read_excel("customer_data.xlsx")
 
@@ -36,4 +95,4 @@ def main():
     print("  - customer_data_invalid/customer_data_invalid.xlsx")
 
 if __name__ == "__main__":
-    main()
+    main()"""
